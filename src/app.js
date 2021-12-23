fe60a65d1689e2c1840f5a1466c3d1c25f3c6d9b
DanielRyanDev <daniel.jgryan2016@gmail.com>
2021-12-23 10:43:03 +0000

    added CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 require("./db/connection");
 const { Sequelize } = require("sequelize");
 const yargs = require("yargs");
-const { addMovie, deleteMovie } = require("./movie/movie.functions");
+const { addMovie, deleteMovie, updateMovie, findMovie } = require("./movie/movie.functions");
 
 const app = async (args) => {
     try {
         let movieObj = {title: args.title, director: args.director}
         if (args.add) {
             await addMovie(movieObj);
-        }   if (args.delete) {
+        }   else if (args.find) {
+            await findMovie(movieObj);
+        }   else if (args.update) {
+            await updateMovie(movieObj.director);
+        }   else if (args.delete) {
             await deleteMovie(movieObj);
         }
     } catch (error) {
